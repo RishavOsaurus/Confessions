afacc60bb4633303af5c9a6ac2a79fcb08dd869a
Rishav Chapagain <gainrishavchap@gmail.com>
2024-05-20 23:06:07 +0545

    Footer Edited

--- html/footer.php
+++ html/footer.php
@@ -1,4 +1,9 @@
-<footer style="font-family: serif ;width: 100dvw; height: fit-content; padding: 2px 0; background-color: white; text-align: center;">By: <a href="">Rishav Chapagain</a> and <a href="">Anish K. Neupane</a> © <span id="date"></span></footer>
+<footer style="font-family: serif ;width: 100dvw; height: fit-content; padding: 2px 0; background-color: white; text-align: center;">
+&copy; <span id="date"></span>
+by: <a href="https://github.com/RishavOsaurus" target="_blank">Rishav Chapagain</a> and <a href="https://github.com/anishkn04" target="_blank">Anish K. Neupane</a>
+
+
+</footer>
 <script>
     let date = new Date();
     document.getElementById("date").innerText = date.getFullYear();
